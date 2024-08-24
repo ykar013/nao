7d6cc352f22e6e3114ffb534e320bd069c18ce46
ykaro <icarosilvaaaaa31@gmail.com>
2024-08-23 22:52:16 -0300

    sei la

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 function togglemode() {
-    const html = document.documentElement
-html.classList.toggle ('light')
+    const html= document.documentElement
+    if (html.classList.contains('light'))  {
+        html.classList.remove ('light')
+     } else {
+        html.classList.add ('light')
 
 const img = document.querySelector("#profile img")
 
-if (html.classList.contains('light'))  {
+img.setAttribute("scr:","./assets/gojo.png")
 
-img.setAttribute("scr","./assets/avatar-light.png")
-} else {
 
-img.setAttribute("scr","./assets/avatar.png")
+img.setAttribute("scr:","./assets/sukuna.png")
 }
 
 
